f01412a1f4f89260c6c2815858d079c88af1dd34
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-09-27 15:57:34 +1300

    Add express html server

---
+++ app.js
@@ -0,0 +1,19 @@
+// Fields
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, "assets")))
+
+// app.set("view engine","ejs")
+// app.set("views", path.join(__dirname, "views"))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(port, () => {
+    console.log(`Express http server running on port ${port}.`)
+})
