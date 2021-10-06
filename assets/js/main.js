59dc1e399e1167a7d93611143448ed78b74bb0d3
Steffen Geving <GEVIS1@student.op.ac.nz>
2021-10-06 18:43:24 +1300

    Fix content grid layout
    Now all 5 items line up properly in the grid and are hidden behind the
    splash page before the user enters

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,53 @@
-// Add js here!
-
 console.log("Successfully loaded the JS file!")
+
+// Fetch the splash screen buttons =========================
+const splash = document.querySelector(".splash")
+const splashButtons = {
+    minus: splash.querySelectorAll("#minus").item(0),
+    plus: splash.querySelectorAll("#plus").item(0)
+}
+
+const errorButtons = {
+    minus: splash.querySelectorAll("#minus").item(1),
+    plus: splash.querySelectorAll("#plus").item(1)
+}
+
+console.log(`splashButtons`, splashButtons)
+console.log(`errorButtons`, errorButtons)
+//====================================================
+
+// Fetch the content divs ============================
+
+const content = document.querySelectorAll(".box")
+
+//====================================================
+
+// Button click eventListeners
+splashButtons.minus.addEventListener("click", event => {
+    // Show error screen and hide splash    
+})
+
+splashButtons.plus.addEventListener("click", () => {
+    // Hide splash and make it non-clickable
+    splash.style.visibility = "hidden"
+    console.log("Hid splash element!", splash)
+    
+})
+
+// errorButtons.minus.addEventListener("click", () => {
+    
+// })
+
+// errorButtons.plus.addEventListener("click", () => {
+    
+// })
+
+// Content div event listeners ========================
+content.forEach((div,key) => {
+    div.addEventListener("click", () => {
+        
+        
+
+    })
+})
+// ====================================================
