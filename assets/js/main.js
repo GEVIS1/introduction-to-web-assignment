00e9de47a77b8bd82d408b26443f0cf1b37d09a5
Steffen Geving <GEVIS1@student.op.ac.nz>
2021-10-06 21:43:59 +1300

    Add animation on grid content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 console.log("Successfully loaded the JS file!")
 
-// Fetch the splash screen buttons =========================
+// Fetch elements =========================
 const splash = document.querySelector(".splash")
 const splashButtons = {
     minus: splash.querySelectorAll("#minus").item(0),
@@ -12,15 +12,19 @@ const errorButtons = {
     plus: splash.querySelectorAll("#plus").item(1)
 }
 
+const grid = document.querySelector(".grid")
+const content = document.querySelectorAll(".box")
+const copy = document.querySelector(".copy")
+const endPos = {
+    gridArea: "1 / 1 / span 4 / span 5"
+}
+
 console.log(`splashButtons`, splashButtons)
 console.log(`errorButtons`, errorButtons)
+console.log(`content`, content)
+console.log(`copy`, copy)
 //====================================================
 
-// Fetch the content divs ============================
-
-const content = document.querySelectorAll(".box")
-
-//====================================================
 
 // Button click eventListeners
 splashButtons.minus.addEventListener("click", event => {
@@ -43,11 +47,56 @@ splashButtons.plus.addEventListener("click", () => {
 // })
 
 // Content div event listeners ========================
-content.forEach((div,key) => {
-    div.addEventListener("click", () => {
-        
-        
+content.forEach((div) => {
+    div.addEventListener("click", event => {
+        console.log("You clicked on:", event.target)
+        console.log("Changing column layout!")
+        const startPos = findGridPos(event.target)
 
+        content.forEach((div) => {
+            div.style.gridColumn = "6 / span 1"
+            div.style.gridRow = "span 1"
+        })
+
+        animate(event.target, startPos)
     })
 })
 // ====================================================
+
+// Functions ==========================================
+const animate = (div, startPos) => {
+    div.style.gridArea = startPos.gridArea
+
+    let animation = div.animate([
+    {
+        gridArea: startPos.gridArea
+    },
+    {
+        gridArea: endPos.gridArea
+    }
+    ], 300)
+
+    animation.onfinish = () => {
+        div.style.gridArea = endPos.gridArea
+    }
+}
+
+const findGridPos = div => {
+    const index = Array.from(content).indexOf(div)
+    console.log("The index of the clicked div is:", index)
+    let gridArea
+
+    if (content[index].style.gridArea == undefined) {
+        const rowStart = index < 3 ? 1 : 3
+        const rowSpan = 2
+        const rowItems = index < 3 ? 0 : 2
+        const columnSpan = index < 3 ? 3 : 2
+        const columnStart = index - rowItems
+        gridArea = `${rowStart} / ${columnStart} / span ${rowSpan} / span ${columnSpan}`
+    }
+    else {
+        gridArea = div.style.gridArea
+    }
+    console.log(`gridArea`, gridArea)
+    return gridArea
+}
